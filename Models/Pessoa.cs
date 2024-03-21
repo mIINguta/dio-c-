60e98d87003fa71c0df9c39f9d1ba76c6e7160ff
mIINguta <gustavo.minguta@gmail.com>
2024-03-21 14:10:51 -0300

    Criando nossa classe no código

---
+++ Models/Pessoa.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemplosFundamentos.Models
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+
+        public void Apresentar()
+        {
+            Console.WriteLine($"Olá, me chamo {Nome}, tenho {Idade} anos de idade");
+        }
+    }
+}
--- obj/Debug/net8.0/ExemplosFundamentos.AssemblyInfo.cs
+++ obj/Debug/net8.0/ExemplosFundamentos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemplosFundamentos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4d45dcf963c50b6a0dcad843a64213dec57a939")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemplosFundamentos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemplosFundamentos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
