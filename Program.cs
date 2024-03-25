e348c5e2fd5cd44f2834aa65bb1c2f37e491e21d
mIINguta <gustavo.minguta@gmail.com>
2024-03-25 14:52:38 -0300

    Criando menu interativo

--- Program.cs
+++ Program.cs
@@ -2,14 +2,64 @@
 using ExemplosFundamentos.Models;
 
 
-int i, soma = 0;
-do{
-    Console.WriteLine("Digite um número: - (0) para parar ");
-    i = Convert.ToInt32(Console.ReadLine());
-    soma += i;
-}
-while(i != 0 );
-    Console.WriteLine($"O resultado é: {soma}");
+
+
+string opcao;
+
+while(true){
+    Console.Clear();
+    Console.WriteLine("Digite a sua opção: ");
+    Console.WriteLine("1 - Cadastrar cliente");
+    Console.WriteLine("2 - Buscar cliente");
+    Console.WriteLine("3 - Apagar cliente");
+    Console.WriteLine("4 - Encerrar");
+
+
+    opcao = Console.ReadLine();
+    
+    
+    switch(opcao){
+        case "1":
+            Console.WriteLine("Cadastro de cliente");
+            break;
+        case "2":
+            Console.WriteLine("Busca de cliente");
+            break;
+        case "3":
+            Console.WriteLine("Apagar de cliente");
+            break;
+        case "4":
+            Console.WriteLine("Encerrar");
+            Environment.Exit(0);
+            break;
+        
+        
+        default:
+            Console.WriteLine("Essa opção não existe!");
+            break;
+        }
+
+        
+
+
+    }
+
+
+
+
+
+
+
+
+
+// int i, soma = 0;
+// do{
+//     Console.WriteLine("Digite um número: - (0) para parar ");
+//     i = Convert.ToInt32(Console.ReadLine());
+//     soma += i;
+// }
+// while(i != 0 );
+//     Console.WriteLine($"O resultado é: {soma}");
 
 
 
--- obj/Debug/net8.0/ExemplosFundamentos.AssemblyInfo.cs
+++ obj/Debug/net8.0/ExemplosFundamentos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemplosFundamentos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af044762c5ffd578e1605c1bb1854f5d94c7b5d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ce563c87a5559b05935947bd5aaa27ff40877dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemplosFundamentos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemplosFundamentos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
