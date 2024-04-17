a781b886e2a3a3bffd0f29cc432a69653114f9bb
mIINguta <gustavo.minguta@gmail.com>
2024-04-17 11:09:48 -0300

    Criando API, controller e modificando código

---
+++ webApi/Program.cs
@@ -0,0 +1,22 @@
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers(); // para que ele possa entender a parte dos controllers
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseHttpsRedirection();
+app.MapControllers(); // para mapear as rotas fornecidas por cada controller
+app.Run();
+
+
