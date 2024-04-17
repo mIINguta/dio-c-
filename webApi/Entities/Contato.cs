f57fe61d2bf579eba3ac28a8c5969e4d79c8119c
mIINguta <gustavo.minguta@gmail.com>
2024-04-17 14:28:42 -0300

    Criando Entities

---
+++ webApi/Entities/Contato.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webApi.Entities
+{
+    public class Contato
+    {
+        public Contato(string id, string nome, string telefone, bool ativo){
+            Id = id;
+            Nome = nome, 
+            Telefone = telefone,
+            Ativo = ativo;
+        }
+    
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
