7ef53ac73887883becc65a6ef7b281bda46905ac
mIINguta <gustavo.minguta@gmail.com>
2024-04-18 15:06:22 -0300

    Entendendo migrations

--- webApi/obj/Debug/net8.0/webApi.AssemblyInfo.cs
+++ webApi/obj/Debug/net8.0/webApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc361031ed457b2df4c0e6af892e24d9e50ef88d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d27ec28d951ea8be0cae63112a1a3e8dd830f595")]
 [assembly: System.Reflection.AssemblyProductAttribute("webApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
