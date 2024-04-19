c7c830ef519392c3fda88e644da0c3033cc58e1e
mIINguta <gustavo.minguta@gmail.com>
2024-04-19 14:02:09 -0300

    Criando EndPoint Update

--- webApi/Controllers/ContatoController.cs
+++ webApi/Controllers/ContatoController.cs
@@ -36,5 +36,24 @@ namespace webApi.Controllers
             
             return Ok(contato);    
         }
+        
+        [HttpPut("{id}")]
+        public IActionResult AtualizarContato(int id, Contato contato){
+            var contatoBanco = _context.Contatos.Find(id);
+
+            if(contatoBanco == null){
+                return NotFound();
+            }
+            
+            contatoBanco.Nome = contato.Nome;
+            contatoBanco.Telefone = contato.Telefone;
+            contatoBanco.Ativo = contato.Ativo;
+
+            _context.Contatos.Update(contatoBanco);
+            _context.SaveChanges();
+            return Ok(contatoBanco);
+
+
+        }
     }
 }
--- webApi/obj/Debug/net8.0/webApi.AssemblyInfo.cs
+++ webApi/obj/Debug/net8.0/webApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08e5cd61aa0b2155f0b8a414cd8e8425b17fd0c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f07f70126c092f347e2da2b988ac7a39a0daa6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("webApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
